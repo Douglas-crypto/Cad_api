b37c3e31d65fb90618f4e1ad9d22ff389e828470
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-04-30 19:22:40 -0300

    Adaptação com body

--- server.js
+++ server.js
@@ -3,19 +3,23 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json()); // Middleware para analisar o corpo da requisição como JSON
+
 const users = [];
 
 app.post('/usuarios', (req, res) => {
-
+    // Adiciona um novo usuário ao array
     console.log(req)
 
     res.send('Deu bom!');
 });
 
 app.get('/usuarios', (req, res) => {
+
     res.send('Deu bom!');
 });
 
 app.listen(3000, () => {
+    // Inicia o servidor na porta 3000
     console.log('Servidor rodando na porta 3000');
 });
