4f485de106c6c105bcba2127e6a41cafd1ef133b
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-04-30 18:06:51 -0300

    Continuação do código

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 import express from 'express'
+
+const app = express()
+
+app.get('/usuario', (req, res) => {
+
+    app.send('Hola Mundo')
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
